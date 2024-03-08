d9f7ab4b93934a2d9b22ab5051e88e7495a8ba26
bonniekyd <44518683+bonniekyd@users.noreply.github.com>
2024-03-08 21:14:00 +0600

    Update script.js

--- script.js
+++ script.js
@@ -15,8 +15,25 @@ function confirmSelection() {
 
     if (selectedName) {
         resultDiv.classList.remove('hidden');
-        image.src = selectedName.toLowerCase().replace(/\s+/g, '') + '.png';
-        audio.src = selectedName.toLowerCase().replace(/\s+/g, '') + '.mp3';
+        if (selectedName === 'Anel') {
+            image.src = 'anel.png';
+            audio.src = 'anel.mp3';
+        } else if (selectedName === 'Aliia') {
+            image.src = 'alia.png';
+            audio.src = 'alia.mp3';
+        } else if (selectedName === 'Aru B') {
+            image.src = 'arub.png';
+            audio.src = 'arub.mp3';
+        } else if (selectedName === 'Aru S') {
+            image.src = 'arus.png';
+            audio.src = 'arus.mp3';
+        } else if (selectedName === 'Tomi') {
+            image.src = 'tomi.png';
+            audio.src = 'tomi.mp3';
+        } else if (selectedName === 'Dana') {
+            image.src = 'dana.png';
+            audio.src = 'dana.mp3';
+        }
     } else {
         alert('Please select a name before confirming.');
     }
